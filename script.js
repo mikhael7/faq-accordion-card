88378645cf16c65b98894c1fb22eaed18c148ddc
mikhael <mikhaellion@gmail.com>
2022-12-19 18:08:05 +0700

    update

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 const buttonAccordion = document.getElementsByClassName("accordion");
 console.log(buttonAccordion);
+
+for (let val of ratingNum.length) {
+  val.addEventListener("click", (item) => {
+    for (let val1 of ratingNum.length) {
+      val1.classList.contains("isChecked");
+      val1.classList.remove("isChecked");
+    }
+    val.classList.add("isChecked");
+  });
+}
+
+thankButton.addEventListener("click", () => {
+  thankContainer.classList.remove("hidden");
+  ratingContainer.classList.add("hidden");
+});
