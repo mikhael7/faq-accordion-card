1cda2afdd8c985fc620820a28f9ef6642b3b9f58
mikhael <mikhaellion@gmail.com>
2022-12-20 14:02:23 +0700

    update

--- script.js
+++ script.js
@@ -5,12 +5,14 @@ for (let i = 0; i < buttonAccordion.length; i++) {
     console.log(buttonAccordion[i].firstElementChild.firstElementChild);
     if (buttonAccordion[i].nextElementSibling.classList.contains("hidden")) {
       buttonAccordion[i].nextElementSibling.classList.remove("hidden");
+      buttonAccordion[i].classList.add("text-black", "bold");
       buttonAccordion[i].firstElementChild.firstElementChild.classList.add(
         "flip"
       );
       return;
     }
     buttonAccordion[i].nextElementSibling.classList.add("hidden");
+    buttonAccordion[i].classList.remove("text-black", "bold");
     buttonAccordion[i].firstElementChild.firstElementChild.classList.remove(
       "flip"
     );
